539123abb5742921573716f78b5a44365f1c9d3c
Maj Soklič <majs.1236@gmail.com>
2023-09-28 12:36:57 +0200

    Remove whitespacess, add typehints

--- Activities/clothesShop.py
+++ Activities/clothesShop.py
@@ -1,27 +1,29 @@
-from Utilities.interface import slow_print, display_stats, clsscr
+from typing import Dict
+
+from Utilities.interface import slow_print, clsscr
 from Utilities.loader import  load_json
 from Player.Playerstats import update_player_stats
 
-def load_clothes_json():
+def load_clothes_json() -> Dict[str, int]:
     clothes_shop = load_json("./Activities/clothesShop.json")
     clothes_in_shop = clothes_shop.get("clothes", {})
     return clothes_in_shop
 
-def clothes_Shop():
-    """Go to clothes shop"""
+def clothes_Shop() -> None:
+    """Go Shopping for clothes"""
 
     #loads json files
     player_stats = load_json("player_stats.json")
 
     slow_print("You walk into the shop and start picking out clothes.")
     clsscr()
-    
+
     while True: #This loop begins the shopping process
         clsscr()
         will_buy = (input("Do you wish to shop? ")).lower() #asks player if they wish to shop (will_buy is meant to mean "will you buy something")
 
         if will_buy == "yes": #asks player if they wish to buy some clothes
-    
+
             for idx, (wear, price) in enumerate(load_clothes_json().items(), start = 1): #display shopping options to player
                 slow_print(f"{wear} costs {price} - [{idx}]")
 
@@ -31,10 +33,10 @@ def clothes_Shop():
                 clothes_price = load_clothes_json().get(buy_choice, 0) #gets the value of the item you bought
                 update_player_stats(player_stats, clothes_price, looks='plus', money='minus') #updates plr stats, removing money and adding to looks
                 slow_print(f"you bought {buy_choice} for ${clothes_price}.", sleepfor=1) 
-            
+
             else: #if the product is not in the dict, it reminds you so
                 slow_print("Please enter a valid option!", sleepfor=1)
-                
+
         else:
             slow_print("*You head home*", sleepfor=1) #sends player back to menu
             break
