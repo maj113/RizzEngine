077ed3a12d2d49185c804079a019340e4fa07be3
Maj Soklič <majs.1236@gmail.com>
2023-09-29 17:33:32 +0200

    enhance: use clear screen when necessary

--- Activities/clothesShop.py
+++ Activities/clothesShop.py
@@ -15,19 +15,19 @@ def clothes_Shop() -> None:
     #loads json files
     player_stats = load_json("player_stats.json")
 
-    slow_print("You walk into the shop and start picking out clothes.")
-    clsscr()
+    slow_print("You walk into the shop and start picking out clothes.", sleepfor=1)
 
     while True: #This loop begins the shopping process
         clsscr()
-        will_buy = (input("Do you wish to shop? ")).lower() #asks player if they wish to shop (will_buy is meant to mean "will you buy something")
+        slow_print("Do you wish to shop? ", newlineend=False)
+        will_buy = input().strip().lower() #asks player if they wish to shop (will_buy is meant to mean "will you buy something")
 
         if will_buy == "yes": #asks player if they wish to buy some clothes
-
+            clsscr()
             for idx, (wear, price) in enumerate(load_clothes_json().items(), start = 1): #display shopping options to player
                 slow_print(f"{wear} costs {price} - [{idx}]")
-
-            buy_choice = (input("Input the name of the product you wish to buy: ")) #The buy_choice variable prompts the player 
+            slow_print("\nInput the name of the product you wish to buy: ", newlineend=False)
+            buy_choice = input().strip().lower() #The buy_choice variable prompts the player 
 
             if buy_choice in load_clothes_json(): #checks if the desired product is in the dict (this dict is inside clothesShop.json)
                 clothes_price = load_clothes_json().get(buy_choice, 0) #gets the value of the item you bought
