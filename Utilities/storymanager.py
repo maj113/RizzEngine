8bcabc9e8880656fd89e39d9ccb507d2687a4a3d
Maj Soklič <majs.1236@gmail.com>
2023-10-02 22:42:00 +0200

    refactor compare_stats

--- Utilities/storymanager.py
+++ Utilities/storymanager.py
@@ -13,13 +13,7 @@ def check_stats() -> dict:
     player_stats = load_json(player_stats_file)
     return player_stats
 
-# Use the process_directory function from loader.py to load character stats
-def check_stories() -> Dict[str, dict]:
-    characters_stats = process_directory(stories_directory)
-    return characters_stats
-
-# Use the process_directory function from loader.py to load character stats
-def compare_stats(player_stats: dict=None, characters_stats: Dict[str, dict] =None) -> str:
+def compare_stats(player_stats=None, characters_stats=None) -> str:
     """
     Compare player's stats with character stats and return available characters as a formatted string.
 
@@ -33,31 +27,25 @@ def compare_stats(player_stats: dict=None, characters_stats: Dict[str, dict] =No
     if not player_stats:
         player_stats = check_stats()
     if not characters_stats:
-        characters_stats = check_stories()
+        characters_stats = process_directory()
 
-    # Initialize an empty list to store available characters
-    available_characters = []  
+    available_characters = []
 
-    # Iterate through the character data stored in the characters_stats dictionary
     for character_name, character_data in characters_stats.items():
-        # Ensure you access the "stats" field within the character data correctly
         character_stats = character_data.get(character_name, {}).get("stats", {})
 
-        # Check if the player's stats meet or exceed the character's stats
         if (
-            player_stats["jacked"] >= character_stats.get("jacked", 0) and
-            player_stats["attraction"] >= character_stats.get("attraction", 0) and
-            player_stats["looks"] >= character_stats.get("looks", 0)
-        ):  
-            # Add the character to the available_characters list
-            available_characters.append(character_name)  
+            player_stats.get("jacked", 0) >= character_stats.get("jacked", 0) and
+            player_stats.get("attraction", 0) >= character_stats.get("attraction", 0) and
+            player_stats.get("looks", 0) >= character_stats.get("looks", 0)
+        ):
+            available_characters.append(character_name)
 
-    # Convert the list of character names into a formatted string
     if not available_characters:
         return "No one, get better rizz bro."
-    formatted_characters = ', '.join(available_characters)
 
-    return formatted_characters  # Return the formatted string
+    formatted_characters = ', '.join(available_characters)
+    return formatted_characters
 
 def check_saves(character_name: str) -> str:
     character_story_data = load_json(os.path.join(stories_directory, f"{character_name}.json"))
