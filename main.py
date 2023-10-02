3a4edec88d8483327e48a1ba556ab64c021f13bf
Maj Soklič <majs.1236@gmail.com>
2023-10-02 22:43:14 +0200

    init the Mainmenu class in main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Utilities.interface import mainmenu, slow_print, clrscr
+from Utilities.interface import Mainmenu, slow_print, clrscr
 from Player.Playerstats import get_player_name
 
 clrscr()
@@ -8,4 +8,4 @@ if get_player_name():
     slow_print(f"Welcome back {get_player_name()}!", sleepfor=1) 
 
 
-mainmenu()
+Mainmenu().mainmenu()
