3f7e10183413af496dc70c9acec37207e4542f2e
TooDeepForSleep <2deep4sleep@gmail.com>
2023-10-03 18:31:31 +0200

    fixed stupid code for rizzducation

--- Activities/rizzducation.py
+++ Activities/rizzducation.py
@@ -3,19 +3,11 @@ from Utilities.loader import  load_json
 from Player.Playerstats import update_player_stats
 import random
 
+#this is the money required to take one rizz class
+required_money = 30
 
-
-def take_rizz_classes() -> None:
-    """Take rizz classes from a trained professional (costs $30)"""
-    
-    # Load player stats
-    player_stats = load_json("player_stats.json")
-
-    #this is the money required to take one rizz class
-    required_money = 30
-
-    #random semi humorous qotes that appear while the player waits for the classes to end
-    quotes = {
+#random semi humorous qotes that appear while the player waits for the classes to end
+quotes = {
         1: " remembering the digits of pi",
         2: " scrolling phone cuz teacher boring",
         3: " quoting Napoleon",
@@ -26,35 +18,37 @@ def take_rizz_classes() -> None:
         8: " yassifying you"
     }
 
-    # Get the desired workout time from the player
-    while True:
-        try:
-            slow_print(
-                "You decide to go take rizz classes tought by your school janitor. ", newlineend=False
-            )
-            clrscr()
+def take_rizz_classes() -> None:
+    """Take rizz classes from a trained professional (costs $30)"""
+    
+    # Load player stats
+    player_stats = load_json("player_stats.json")
 
-            if required_money <= player_stats["money"]:
-                break
-            else:
-                return slow_print("The janitor takes offense to your lackluster amount of money. BYE!", sleepfor=2)
-
-        except ValueError:
-            clrscr()
-            slow_print("Input a number next time, jackass!", sleepfor=2, clear=True)
-
-
-    # Display a workout animation
-    for _ in range(5):
-        slow_print(random.choice(quotes), sleepfor=8, newlineend=False)
-    clrscr()
-
-    # Update player stats
-    update_player_stats(player_stats, 10, attraction='plus', money='minus')
-
-    # Show post-workout message and updated stats
     slow_print(
-        "By some miracle you managed to remember something.", sleepfor=2, newlineend=False
-    )
-    display_stats()
+        "You decide to go take rizz classes tought by your school janitor. ", newlineend=False
+        )
+    clrscr()
+    #checks if player has enough money
+    if required_money <= player_stats["money"]:
+
+        # Display a taking notes animation
+        for i in range(5):
+            slow_print(random.choice(quotes), sleepfor=8, newlineend=False)
+            clrscr()
+
+        # Update player stats
+        update_player_stats(player_stats, 10, attraction='plus', money='minus')
+
+        # Show post-class message and updated stats
+        slow_print(
+            "By some miracle you managed to remember something.", sleepfor=2, newlineend=False
+            )
+        display_stats()
+
+    else: slow_print("The janitor takes offense to your lackluster amount of money. BYE!", sleepfor=2)
+
+
+
+
+
 
